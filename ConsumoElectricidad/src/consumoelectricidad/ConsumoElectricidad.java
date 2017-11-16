55dbec3e5d6b926ca575feb1714380601a374651
dmartinezsaa <dani@dani-GE60-2QD>
2017-11-16 14:24:47 +0100

    inicio

---
+++ ConsumoElectricidad/src/consumoelectricidad/ConsumoElectricidad.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consumoelectricidad;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dani
+ */
+public class ConsumoElectricidad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Datos p1=new Datos();
+        String codigo=null;
+        while(1==1){
+            p1.setCodigo(JOptionPane.showInputDialog("Introduzca código del cliente:"));
+            codigo=p1.getCodigo();
+            if(codigo.equals("*"))
+                break;
+            p1.setLectura_1(Integer.parseInt(JOptionPane.showInputDialog("Introduzca lectura anterior en Kw/h:")));
+            p1.setLectura_2(Integer.parseInt(JOptionPane.showInputDialog("Introduzca lectura actual en Kw/h:")));
+            p1.setPotencia(Float.parseFloat(JOptionPane.showInputDialog("Introduzca potencia contratada:")));
+            
+            JOptionPane.showMessageDialog(null, "Importe a pagar: "+p1.total());
+        }
+    }
+    
+}
---
+++ ConsumoElectricidad/src/consumoelectricidad/Datos.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consumoelectricidad;
+
+/**
+ *
+ * @author dani
+ */
+public class Datos {
+    String codigo;
+    int lectura_1,lectura_2;
+    float potencia;
+    final private float iva=0.12f;
+    
+    public Datos(){
+        
+    }
+    public Datos(String codigo, int lectura_1, int lectura_2, int potencia) {
+        this.codigo = codigo;
+        this.lectura_1 = lectura_1;
+        this.lectura_2 = lectura_2;
+        this.potencia = potencia;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getLectura_1() {
+        return lectura_1;
+    }
+
+    public int getLectura_2() {
+        return lectura_2;
+    }
+
+    public float getPotencia() {
+        return potencia;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setLectura_1(int lectura_1) {
+        this.lectura_1 = lectura_1;
+    }
+
+    public void setLectura_2(int lectura_2) {
+        this.lectura_2 = lectura_2;
+    }
+
+    public void setPotencia(float potencia) {
+        this.potencia = potencia;
+    }
+    
+    public float total(){
+        float total,importep,ivaimporte;
+        total=(lectura_1+lectura_2)*13.11f;
+        
+        if(potencia<3.3f)
+            importep=224;
+        else if(potencia<4.4f)
+            importep=351;
+        else if(potencia<5.5f)
+            importep=413;
+        else if(potencia<6.6f)
+            importep=517;
+        else
+            importep=626;
+
+        return total+((total+importep)*iva);
+    }
+    
+}
